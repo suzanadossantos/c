d8394d942e78a7bba96074b62771437ff1d090a5
Suzana dos Santos <94690066+suzanadossantos@users.noreply.github.com>
2022-01-12 10:30:55 -0300

    atualização

--- ex003.cpp
+++ ex003.cpp
@@ -6,26 +6,35 @@ using namespace std;
 
 int main(){
 	setlocale(LC_ALL, "ptb"); 
-	
-	int num;
+
+	int num, num_1, num_2;
 	cout<< "---Calculadora--- \n";
 	cout<< "Qual operação você deseja fazer? \n";
 	cout<< "1- Adição ";
 	cout<< "2- Subtração ";
 	cout<< "3- Multiplicação ";
-	cout<< "4- Divisão";
+	cout<< "4- Divisão \n";
 	cout<< "Número: \n";
 	cin>> num;
 	
-	if(num != 0){
+	if (num <= 4){
 		cout<< "Digite o primeiro número: \n";
 		cin>> num_1;
 		cout<< "Digite o segundo número: \n";
-		cin>> num2;
-	} else if(num == 1){
-		
+		cin>> num_2;
+	}  
+	if(num == 1){
+		cout<< "--Resposta-- \n";
+		cout<< num_1 + num_2;
 	} else if(num == 2){
-		
+		cout<< "--Resposta-- \n";
+		cout<< num_1 - num_2;
+	} else if(num == 3){
+		cout<< "--Resposta-- \n";
+		cout<< num_1 * num_2;
+	} else{
+		cout<< "--Resposta-- \n";
+		cout<< num_1 / num_2;
 	}
 	
 }
